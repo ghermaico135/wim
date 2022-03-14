a4bfc298ac429a2297e77ada9b430720ed682197
Mike <ghermaico135@gmail.com>
2022-03-14 17:26:07 +0300

    client validation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,47 @@
-// const loginBox = document.querySelector("#login-box");
-// const registerBox = document.querySelector("#register-box");
-// const registerLink = document.querySelector("#registerLink");
-// const loginLink = document.querySelector("#loginLink");
+const form = document.querySelector("#register-form");
+const Fname = document.querySelector("#fname");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const password = document.querySelector("#password");
+const RepeatPassword = document.querySelector("#r_password");
+const gender = document.querySelector("#gender");
 
-// registerLink.addEventListener("click", () => {
-//     registerBox.style.display = "block";
-//     loginBox.style.display = "none";
-// });
+function showError(input, message) {
+    const formInput = input.parentElement;
+    formInput.className = "form-control error";
+    // formInput.innerText = message;
+}
 
-// loginLink.addEventListener("click", () => {
-//     loginBox.style.display = "block";
-//     registerBox.style.display = "none";
-// });
+function showSuccess(input) {
+    const formInput = input.parentElement;
+    formInput.className = "form-control success";
+}
+
+function checkRequired(ArrayInput) {
+    ArrayInput.forEach((input) => {
+        if (input.value.trim() === "") {
+            showError(input, "All field is required");
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function checkEmail(input) {}
+function checkPasswordMatch(input1, input2) {}
+function checkLength(input, max, min) {}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("hello");
+    checkRequired([
+        Fname,
+        username,
+        email,
+        phone,
+        password,
+        RepeatPassword,
+        gender,
+    ]);
+});
