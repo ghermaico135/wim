cf3334e767b5bc9da8bf37e1ffcd6eb2204b7f6a
Mike <ghermaico135@gmail.com>
2022-03-14 15:40:56 +0300

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+// const loginBox = document.querySelector("#login-box");
+// const registerBox = document.querySelector("#register-box");
+// const registerLink = document.querySelector("#registerLink");
+// const loginLink = document.querySelector("#loginLink");
+
+// registerLink.addEventListener("click", () => {
+//     registerBox.style.display = "block";
+//     loginBox.style.display = "none";
+// });
+
+// loginLink.addEventListener("click", () => {
+//     loginBox.style.display = "block";
+//     registerBox.style.display = "none";
+// });
